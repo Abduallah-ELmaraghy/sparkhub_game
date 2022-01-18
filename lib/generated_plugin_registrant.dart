769c0f4b50d293926ae30445f59f284b0e637e64
Mi8aelmasry <abdu.mara8y@gmail.com>
2022-01-18 02:26:23 +0200

    All dependencies upgraded

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -5,6 +5,8 @@
 // ignore_for_file: directives_ordering
 // ignore_for_file: lines_longer_than_80_chars
 
+import 'package:assets_audio_player_web/web/assets_audio_player_web.dart';
+import 'package:flutter_tts/flutter_tts_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
 
@@ -12,6 +14,8 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
 void registerPlugins(Registrar registrar) {
+  AssetsAudioPlayerWebPlugin.registerWith(registrar);
+  FlutterTtsPlugin.registerWith(registrar);
   SharedPreferencesPlugin.registerWith(registrar);
   UrlLauncherPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
